4aa7f613f92e3065dc021e5a169fbaacdf1a23f9
Hanna Johansson <Hanna.Johansson@amal.se>
2023-03-29 12:28:09 +0200

    lektion 2

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 console.log("meddelande från en annan fil");
 
 
+let url = decodeURI(window.location.href);
+console.log(url);
+let urlParts = url.split("?");
+console.log(urlParts[1]);
+let dataPairs = urlParts[1].split("&");
+
+for (var i = 0; i < dataPairs.length; i++) {
+  console.log(dataPairs[i]);
+  let keyPair = dataPairs[i].split("=");
+  console.log("input" + keyPair[0]);
+  document.getElementById("input" + keyPair[0]).textContent += " " + keyPair[1];
+}
+
+
 function buttonPressed() {
   console.log("någon har tryckt på knappen");
   let redSquare = document.getElementById("red");
